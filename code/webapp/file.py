115b3ecb82208e1ced1dcd4a6ef0d7ed33c6ed16
Eduardo <educa95@gmail.com>
2020-10-16 18:48:54 +0200

    implemented tokenize/untokenize

--- code/webapp/file.py
+++ code/webapp/file.py
@@ -2,21 +2,49 @@ from flask import Flask, redirect, url_for, render_template, request
 import difflib
 from gector.model import load_model
 from gector.predict import predict_for_string
+import spacy
+from nltk.tokenize.treebank import TreebankWordDetokenizer
 
-app = Flask(__name__) 
+app = Flask(__name__)
+
+nlp = spacy.load("en_core_web_sm")
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/predict")
 def predict():
     model = load_model(vocab_path='./gector/data/output_vocabulary/',
                    model_paths=['./static/nn_models/xlnet_0_gector.th'],
                    model_name="xlnet")
-    text = request.args.get('jsdata')
-    corrected_text = predict_for_string(text, model)
-    return show_diff(text, corrected_text)
+    user_input = request.args.get('jsdata')
+    print("'user input'", user_input)
+    tokenized_string = tokenize(user_input)
+    print("'model input'", tokenized_string)
+    corrected_text = predict_for_string(tokenized_string, model)
+    print("'model output'", corrected_text)
+    output_text = untokenize(corrected_text)
+    print("'user output'", output_text)
+    return show_diff(user_input, output_text)
+
+
+def tokenize(text):
+    """
+    input plain text string,
+    outputs tokenized string with spaces
+    """
+    doc = nlp(text)
+    return " ".join([token.text for token in doc])
+
+
+def untokenize(tokens):
+    """
+    input tokenized string with spaces
+    outputs plain text string
+    """
+    return TreebankWordDetokenizer().detokenize(tokens.split(" "))
 
 
 def show_diff(text, n_text):
